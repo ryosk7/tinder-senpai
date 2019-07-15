82cfd3fa91ea25fd2e34df734279149edcfeef79
ryosk7 <uchiryo7@gmail.com>
2019-07-16 00:47:19 +0900

    リファクタリング、dotenvを有効化

--- slack.py
+++ slack.py
@@ -3,13 +3,16 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-endpoint = "SLACK_WEB_HOOK_ENDPOINT"
-WEB_HOOK_URL = "https://hooks.slack.com/services/{}".format(endpoint)
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
+endpoint = os.environ.get("SLACK_WEB_HOOK_ENDPOINT")
+WEB_HOOK_URL = "https://hooks.slack.com/services/{}".format(endpoint)
 def getImageUrlForSlack(imageUrl):
   requests.post(WEB_HOOK_URL, data = json.dumps({
     'text': imageUrl,  #通知内容
     'username': u'tinder先輩',  #ユーザー名
-    'icon_emoji': u':smile_cat:',  #アイコン
+    'icon_emoji': u':new_moon_with_face:',  #アイコン
     'link_names': 1,  #名前をリンク化
   }))
--- tinder.py
+++ tinder.py
@@ -7,7 +7,6 @@ import requests
 import json
 
 load_dotenv(verbose=True)
-
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
@@ -32,10 +31,10 @@ with requests.Session() as s:
 	s.headers.update(headers)
 
 	# 位置情報の登録
-	location = {"lat": 35.658034, "lon": 139.701636} # 渋谷スクランブル交差点
+	location = {"lat": 35.684669, "lon": 139.697098} # ギークハウス新宿の緯度・経度
 	ping = s.post("https://api.gotinder.com/v2/meta",
 				 params=json.dumps(params))
-	count=0
+	count = 0
 	while count < 101:
 		# 周囲のユーザーを取得
 		users = s.post("https://api.gotinder.com/user/recs")
@@ -44,42 +43,15 @@ with requests.Session() as s:
 			# 右スワイプ
 			s.get("https://api.gotinder.com/like/{}".format(id))
 			print(user["name"],"❤️")
-			text = "あへぇ"
-			getImageUrlForSlack(text)
-			# FIXME: Slackの通知がまだうまく行かない
-			count+=1
+			for image in user["photos"]:
+				sendImage = ''
+				image = image['url']
+				if image.endswith(".webp"):
+					sendImage = image.replace(".webp",".jpg") #.webpを.jpgに変換
+			getImageUrlForSlack(sendImage)
+			count += 1
 			if count == 100:
 				print("done!!")
 	
 	# TODO: Firebase使って画像保存
-
   # NOTE: 以下はやりたいこと。25歳以下だけスワイプとか、写真を何枚か取得するとか色々。
-	# FIXME: エラーが出るのでコメントアウト。（そのうちいらなくなって消すかも）
-
-	# 周囲のユーザーを取得
-	# users = s.post("https://api.gotinder.com/user/recs")
-	# userList = []
-	# for user in json.loads(users.text)["results"]:
-	# 	# print(user["photos"])
-	# 	userInfo = []
-	# 	userInfo.append(user["name"])
-	# 	userInfo.append(user["_id"])
-	# 	id = user["_id"]
-	# 	userInfo.append(user["distance_mi"])
-	# 	age = user["distance_mi"]
-	# 	for image in user["photos"]:
-	# 		# print(image["url"])
-	# 		# userInfo.append(image["url"])
-
-	# 		image = image['url']
-	# 		if image.endswith(".webp"):
-	# 			image = image.replace(".webp",".jpg")
-	# 			userInfo.append(image)
-	# 		# 右スワイプ
-	# 		if image is not None and age <= 25:
-	# 			s.get("https://api.gotinder.com/like/{}".format(id))
-	# 			userInfo.append("Liked")
-	# 	userList.append(userInfo)
-			
-	# for user in userList:
-	# 	print(user)
